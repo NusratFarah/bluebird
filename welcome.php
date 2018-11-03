b4d4de6caaa087961bfcab9c23da9df729784f99
Rakibul Islam <24943853+sheikhRakib@users.noreply.github.com>
2018-11-03 22:35:40 +0600

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -1,31 +1,61 @@
+<?php 
+	session_start();
+?>
+<!DOCTYPE html>
 <html>
-<html>
-<head><title>Welcome</title></head>
-<form>
-<table border="1"align="center" width="60%" >
-	
-			<tr background="hotel.jpg"><th colspan="3" align="right" width="60%">
-			<label style="font-family:Algerian" ><center><h1>Blue Bird<h1><center></label>
-			</tr>
-			<tr background="white.jpg">
-				<td align="center"> 
-				     <a href="welcome.php">Home</a> <label>||</label>
-				     <a href="promotional.php">Promotional Offers</a> <label>||</label>
-					 <a href="services.php">Services</a></li> <label>||</label>
-				     <a href="About.php">About The Hotel</a>  <label>||</label>
-					 <a href="login.php">Login</a> <label>||</label>
-					 
-					 
-				</td>
-				 
-			</tr>
-			<tr>
-			     <td colspan="3"><img src= "hotel.jpg" width="100%"></td>
-			</tr>
-			
-			<tr>
-			     <th background="hotel.jpg"><h3>www.bluebird.com</h3>
-		    </tr>
-	</table>
-</form>
+<head>
+	<title>Welcome</title>
+</head>
 
+<body>
+	<table border="1"align="center" width="60%" >
+		<tr background="hotel.jpg">
+			<th colspan="3" align="right" width="60%">
+				<h1 style="font-family: Algerian; text-align: center;">Blue Bird</h1>
+			</th>
+		</tr>
+
+		<tr background="white.jpg">
+			<td align="center"> 
+				<span> || </span>
+				<a href="welcome.php">Home</a> 
+				<span> || </span>
+				<?php 
+					if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == "true") {
+						echo '<a href="promotional.php">Promotional Offers</a>
+							<span> || </span>';
+					}
+				?>
+				
+				<a href="services.php">Services</a></li>
+				<span> || </span>
+				<a href="About.php">About The Hotel</a>
+				<span> || </span>
+				<?php  
+					if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == "true") {
+						echo '<a href="logout.php">Logout</a>';
+					}
+					else{
+						echo '<a href="login.php">Login</a>';
+					}
+				?>
+				<span>||</span>
+				
+			</td>
+		</tr>
+
+		<tr>
+			<td colspan="3">
+				<img src= "hotel.jpg" width="100%">
+			</td>
+		</tr>
+			
+		<tr>
+			<th background="hotel.jpg">
+				<h3>www.bluebird.com</h3>
+			</th>
+		</tr>
+	</table>
+
+</body>
+</html>
